14e4c7b1e5ded3603fd9f09c872b9203ba91df95
esack7 <isaacheist@gmail.com>
2018-07-31 18:52:38 -0700

    rewrote parse_chordpro.js and tests, still some bugs

--- js/utils/parse_chordpro.js
+++ js/utils/parse_chordpro.js
@@ -1,44 +1,77 @@
-module.exports = chordPro => {
-  let parsed = '';
-  const chunkArr = chordPro.split('\n\n');
-  chunkArr.map(ele => {
-    const chordArr = ele.split('\n');
-    chordArr.map(idx => {
-      const eachLine = idx.split('');
-      let chord = '';
-      let text = '';
-      let check = true;
-      let a;
-      let b;
-      for (let i = 0; i < eachLine.length; i += 1) {
-        if (eachLine[i] === '[' || eachLine[i] === ']') {
-          check = !check;
-          if (eachLine[i] === '[') {
-            b = i;
-          }
-          if (eachLine[i] === ']') {
-            a = i;
-            i += a - b - 1;
-          }
-        } else if (check) {
-          chord = `${chord} `;
-        } else {
-          chord += eachLine[i];
-        }
-      }
-      check = false;
-      for (let j = 0; j < eachLine.length; j += 1) {
-        if (eachLine[j] === '[' || eachLine[j] === ']') {
-          check = !check;
-        } else if (!check) {
-          text += eachLine[j];
-        }
-      }
-      chord = chord.replace(/\s*$/, '');
-      parsed += `${chord}\n${text}\n`;
-      return parsed;
-    });
+const detectChordpro = line => {
+  if (line.split('').includes('[')) return true;
+  return false;
+};
+
+const detectLyrics = line => {
+  if (
+    line
+      .split(' ')
+      .filter(ele => ele !== '')
+      .map(word => word.includes('['))
+      .includes(false)
+  )
+    return true;
+  return false;
+};
+
+const removeBrackets = line =>
+  line
+    .split('')
+    .filter(char => {
+      if (char === '[') return false;
+      if (char === ']') return false;
+      return true;
+    })
+    .join('');
+
+const splitLine = line => {
+  let chordLine = '';
+  let lyricLine = '';
+  let chordOnly = false;
+  const splitLineArr = line.split('');
+  splitLineArr.map((char, index) => {
+    if (char === '[' || char === ']') {
+      chordOnly = !chordOnly;
+      return null;
+    }
+    if (chordOnly) {
+      chordLine = `${chordLine}${char}`;
+      return null;
+    }
+    if (splitLineArr[index + 1] === '[') {
+      lyricLine = `${lyricLine}${char}`;
+      return null;
+    }
+    chordLine = `${chordLine} `;
+    lyricLine = `${lyricLine}${char}`;
     return null;
   });
-  return parsed.trim();
+  return `${chordLine.trimRight()}\n${lyricLine}`;
+};
+
+module.exports = chordPro => {
+  let parsed = '';
+  const blockArr = chordPro.split('\n\n');
+  blockArr.map(block => {
+    const lineArr = block.split('\n');
+    lineArr.map(line => {
+      if (detectChordpro(line)) {
+        if (detectLyrics(line)) {
+          parsed = `${parsed}\n${splitLine(line)}`;
+          return parsed;
+        }
+        parsed = `${parsed}\n${removeBrackets(line)}`;
+        return parsed;
+      }
+      parsed = `${parsed}\n${line}`;
+      return parsed;
+    });
+    parsed = `${parsed}\n`;
+    return parsed;
+  });
+  console.log(parsed.split('')[0].charCodeAt(1));
+  // Need to figure out logic that will trim or trimRight depending on if first line is chords or not
+  if (parsed.split('')[0] === '\n') return parsed.trim();
+  return parsed.trimRight();
 };
